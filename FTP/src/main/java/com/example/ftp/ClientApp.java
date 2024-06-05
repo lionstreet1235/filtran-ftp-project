9f81633ba247e2070f3fd1911dd12c15bfbd55ae
luciferharvey <116283698+LuciferHarvey@users.noreply.github.com>
2024-06-05 21:32:33 +0700

    Commit test

--- FTP/src/main/java/com/example/ftp/ClientApp.java
+++ FTP/src/main/java/com/example/ftp/ClientApp.java
@@ -48,8 +48,8 @@ public class ClientApp extends Application {
             } catch (IOException e) {
                 e.printStackTrace();
                 javafx.application.Platform.runLater(() -> messageLabel.setText("Failed to connect to server"));
-                System.out.println("Hehehehehehehhehhehe");
             }
+            System.out.println("Fuck");
         }).start();
     }
 }
